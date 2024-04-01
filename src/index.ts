4b98d2d1c7520267d23d7876f7ba0f36a642d0a1
Tharki-God <alokender018@gmail.com>
2024-04-01 06:37:50 +0530

    frick me dude

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,16 @@
 import { Injector, Logger, settings } from "replugged";
 import { defaultSettings } from "./lib/consts";
-import Settings from "./Components/Settings";
+
 import "./styles.css";
 
-export const SettingValues = await settings.init("dev.tharki.ShowNames", defaultSettings);
+export const SettingValues = await settings.init("dev.tharki.VolumeManipulator", defaultSettings);
+
 export const PluginLogger = Logger.plugin("VolumeManipulator");
 export const PluginInjector = new Injector();
 
+import Settings from "./Components/Settings";
 import Injections from "./patches/index";
+
 export const start = (): void => {
   Settings.registerSettings();
   Injections.applyInjections();
